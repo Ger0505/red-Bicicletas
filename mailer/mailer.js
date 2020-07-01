dd442930cc4de0a84e3dae089b4729cfcf3f68c6
Ger0505 <gerzonex360@gmail.com>
2020-06-30 19:52:57 -0500

    Agregación de SendGrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,11 +1,33 @@
-var nodemailer = require('nodemailer');
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'johan.wuckert@ethereal.email',
-        pass: 'BppEXXhrBDjUkSVg7B'
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+let mailConfig;
+if(process.env.NODE_ENV === 'production'){
+    const options = {
+        auth:{
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+}else{
+    if(process.env.NODE_ENV === 'staging'){ //Servidor de prueba
+        console.log('XXXXXXXXXXXXXXXXXXX');
+        const options = {
+            auth:{
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+    }else{ // Utilizar Ethereal para correos locales
+        const mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            }
+        };        
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
